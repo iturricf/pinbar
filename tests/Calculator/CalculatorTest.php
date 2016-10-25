f5e5a18b813ce43c8408b875342976752103d7db
Cristian Iturri <iturri.cf@gmail.com>
2016-10-25 20:04:23 -0300

    Added test for invalid quantity exception

--- tests/Calculator/CalculatorTest.php
+++ tests/Calculator/CalculatorTest.php
@@ -13,32 +13,46 @@ use PHPUnit\Framework\TestCase;
  */
 class CalculatorTest extends Testcase
 {
+    private $calc;
+
+    protected function setUp()
+    {
+        $this->calc = new Calculator();
+    }
+
     public function testSimulateAmount()
     {
-        $calc = new Calculator();
-        $op = $calc->simulateAmount(3000, 109.4);
+        $op = $this->calc->simulateAmount(3000, 109.4);
 
         $this->assertEquals($op->getAmount(), 2953.80);
         $this->assertEquals($op->getTotal(), 3017.16);
 
-        $op = $calc->simulateAmount(3100, 152);
+        $op = $this->calc->simulateAmount(3100, 152);
 
         $this->assertEquals($op->getAmount(), 3040);
         $this->assertEquals($op->getTotal(), 3103.44);
 
-        $op = $calc->simulateQuantity(10, 160);
+        $op = $this->calc->simulateQuantity(10, 160);
 
         $this->assertEquals($op->getAmount(), 1600);
         $this->assertEquals($op->getTotal(), 1662.05);
 
-        $op = $calc->simulateQuantity(27, 98, Operation::TYPE_SELL);
+        $op = $this->calc->simulateQuantity(27, 98, Operation::TYPE_SELL);
 
         $this->assertEquals($op->getAmount(), 2646);
         $this->assertEquals($op->getTotal(), 2582.94);
 
-        $op = $calc->simulateQuantity(10, 178, Operation::TYPE_SELL);
+        $op = $this->calc->simulateQuantity(10, 178, Operation::TYPE_SELL);
 
         $this->assertEquals($op->getAmount(), 1780);
         $this->assertEquals($op->getTotal(), 1717.78);
+    }
+
+    /**
+     * @expectedException \DomainException
+     */
+    public function testSimulateAmountException()
+    {
+        $op = $this->calc->simulateAmount(10, 178);
     }
 }
